3452f3834a6b4c1e1e021f2bfe091ebd695a559f
Inggis Kurnia Trisiawan <inggiskurnia32@gmail.com>
2025-09-05 22:25:29 +0700

    initail init

---
+++ src/main/java/com/example/demo/DemoForAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForAuthApplication.class, args);
+	}
+
+}
